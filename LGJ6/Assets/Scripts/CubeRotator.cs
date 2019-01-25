900119826094f446019807c9dc23e7149dc4dce4
Patszcz <32665871+Patszcz@users.noreply.github.com>
2019-01-25 23:28:07 +0100

    Dottween + CubeRotator
    Dottween + CubeRotator

---
+++ LGJ6/Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class CubeRotator : MonoBehaviour
+{
+    public static CubeRotator Instance;
+
+    public UnityEvent unityEvent;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        unityEvent.Invoke();
+    }
+
+    public void Rotate(Vector2 direction)
+    {
+      //  transform.DORotate(, 1f, RotateMode.FastBeyond360);
+    }
+}
