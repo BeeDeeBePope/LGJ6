0c4448a7680fd01ea7dc1267aad00c6bf38fbf09
Patszcz <32665871+Patszcz@users.noreply.github.com>
2019-01-26 20:10:27 +0100

    Some Material
    Some Material change

--- LGJ6/Assets/Scripts/Environment/MapElements/CoinPickup.cs
+++ LGJ6/Assets/Scripts/Environment/MapElements/CoinPickup.cs
@@ -6,21 +6,10 @@ namespace Environment.MapElements
 {
     public class CoinPickup : MonoBehaviour
     {
-        public float RotationSpeed = 1;
-        private Tweener tweener;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            tweener = transform.DORotateQuaternion(Quaternion.AngleAxis(180, transform.up), RotationSpeed).SetLoops(-1);
-            tweener.Play();
-        }
-
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
-                tweener.Kill();
                 var pl = other.GetComponent<PlayerControler>();
                 pl.AddCoin();
                 Destroy(gameObject);
