7a5ff4b83ce51fe4451b49553bc7a7d0dd66d1f7
Adam Trzeszczak <adam.trzeszczak@hotmail.com>
2019-01-26 09:54:35 +0100

    Adding DeathElement and reordering scripts

---
+++ LGJ6/Assets/Scripts/Environment/MapElements/PlayerKiller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Environment.MapElements
+{
+    public class PlayerKiller : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                
+            }
+        }
+    }
+}
