330d039ea89865b4e2e97b560f06644755809b6f
Patszcz <32665871+Patszcz@users.noreply.github.com>
2019-01-26 19:31:37 +0100

    Pierwsza Wygenerowana mapa
    Pierwsza Wygenerowana mapa

---
+++ LGJ6/Assets/Scripts/Tools/MapFromTileGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapFromTileGenerator : MonoBehaviour
+{
+    public Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string mapa = "";
+        mapa += "{";
+        for (int i = 0; i < 24; i++)
+        {
+            mapa += "{";
+            for (int j = 0; j < 24; j++)
+            {
+                mapa += ReturnValuer(j, -i); mapa += ",";
+            }
+            mapa += "},";
+        }
+        mapa += "},";
+
+        Debug.Log(mapa);
+    }
+
+    int ReturnValuer(int i,int j)
+    {
+        Sprite sprite = tilemap.GetSprite(new Vector3Int(i, j, 0));
+        for (int k = 0; k < MapBuilder.Instance.bioms[0].sprites.Length; k++)
+        {
+            
+            if (MapBuilder.Instance.bioms[0].sprites[k].GetComponentInChildren<SpriteRenderer>().sprite == sprite) return k;
+        }
+        Debug.Log(sprite.name);
+        return 5;
+    }
+}
