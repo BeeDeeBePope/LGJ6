8d2e441c1003682fb9bbea8b7143bd611e679bad
Adam Trzeszczak <adam.trzeszczak@hotmail.com>
2019-01-26 10:55:43 +0100

    Adding fragile plate

---
+++ LGJ6/Assets/Scripts/Environment/MapElements/FragilePlate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Environment.MapElements
+{
+    public class FragilePlate : MonoBehaviour
+    {
+        public GameObject PreVisuals;
+        public GameObject PostVisuals;
+
+        private PlayerKiller pk;
+
+        private void Awake()
+        {
+            PreVisuals.SetActive(true);
+            PostVisuals.SetActive(false);
+            pk = GetComponent<PlayerKiller>();
+            pk.DisableOverride = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                PreVisuals.SetActive(false);
+                PostVisuals.SetActive(true);
+                pk.DisableOverride = false;
+            }
+        }
+    }
+}
--- LGJ6/Assets/Scripts/Environment/MapElements/PlayerKiller.cs
+++ LGJ6/Assets/Scripts/Environment/MapElements/PlayerKiller.cs
@@ -5,8 +5,15 @@ namespace Environment.MapElements
 {
     public class PlayerKiller : MonoBehaviour
     {
+        public bool DisableOverride;
+
         private void OnTriggerEnter(Collider other)
         {
+            if (DisableOverride)
+            {
+                return;
+            }
+
             if (other.CompareTag("Player"))
             {
                 PlayerControler pl = other.GetComponent<PlayerControler>();
